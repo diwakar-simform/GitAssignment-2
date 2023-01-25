9531d0047779b307695362f0bbb39688b1ec8723
Diwakar Kumar <diwakar@simformsolutions.com>
2023-01-25 15:25:47 +0530

    commiting changes in branch1

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     {
         System.out.println("Hello world!");
         System.out.println("Hi, This is for git assignment 2");
+        System.out.println("Hi, this is the branch1, ");
     }
 }
